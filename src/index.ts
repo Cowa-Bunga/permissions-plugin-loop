9e3e8acc82ea0cf802a2cb2a16b56475edd3b329
AfroRichard <richard@labs.ws>
2023-02-07 14:51:00 +0200

    updated error message

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 // loop permission middleware
 export default function loopPermit(permittedRole) {
-  // return a middleware for express
   return (request, response, next) => {
     const { permissions } = request
 
     if (permissions.scopes.includes(permittedRole)) {
-      next(); // role is allowed, so continue on the next middleware
+      next(); // we will allow it, go forth!
     } else {
-      response.status(403).json({message: "Forbidden"}); // user is forbidden
+      response.status(403).json({message: `user does not have the required permission to access this resource: ${permittedRole}`});
     }
   }
 }
