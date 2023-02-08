52e95c396ac6fd536772b31c58bce3c88d90e9cb
AfroRichard <richard@labs.ws>
2023-02-08 09:50:27 +0200

    fetch permissions

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// loop permission middleware
+// middleware for doing role-based permissions
+const jsonwebtoken_1 = require("jsonwebtoken");
+const JWT_SECRETE = process.env.JWT_SECRETE;
 function loopPermit(permittedRole) {
+    // return a middleware
     return (request, response, next) => {
-        const { permissions } = request;
+        const authHeader = request.header('Authorization');
+        const [bearer, token] = authHeader.split(' ');
+        if (bearer !== 'Bearer') {
+            response.status(401).json({
+                message: 'User does not have proper authorization. Please provide a Bearer token authorization'
+            });
+        }
+        const decoded = jsonwebtoken_1.default.verify(token, JWT_SECRETE);
+        const permissions = decoded.permissions;
         if (permissions.scopes.includes(permittedRole)) {
-            next(); // we will allow it, go forth!
+            next(); // role is allowed, so continue on the next middleware
         }
         else {
             response.status(403).json({ message: `user does not have the required permission to access this resource: ${permittedRole}` });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-// loop permission middleware
+// middleware for doing role-based permissions
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRETE: any = process.env.JWT_SECRETE;
+
 export default function loopPermit(permittedRole) {
+  // return a middleware
   return (request, response, next) => {
-    const { permissions } = request
+
+    const authHeader = request.header('Authorization');
+    const [bearer, token] = authHeader.split(' ');
+    if (bearer !== 'Bearer') {
+      response.status(401).json({
+        message:
+          'User does not have proper authorization. Please provide a Bearer token authorization'
+      });
+    }
+    const decoded: any = jwt.verify(token, JWT_SECRETE);
+
+    const permissions = decoded.permissions;
 
     if (permissions.scopes.includes(permittedRole)) {
-      next(); // we will allow it, go forth!
+      next(); // role is allowed, so continue on the next middleware
     } else {
-      response.status(403).json({message: `user does not have the required permission to access this resource: ${permittedRole}`});
+      response.status(403).json({ message: `user does not have the required permission to access this resource: ${permittedRole}` });
     }
-  }
+  };
 }
